0d6cdcff45b025dcd8ea165646bbba62fd7d2417
Philip On <nakurunet@gmail.com>
2016-12-02 22:20:01 +0300

    update 1

--- httpapi.py
+++ httpapi.py
@@ -1,24 +1,28 @@
 import httplib
 from urllib2 import Request, urlopen, URLError
- 
-conn = httplib.HTTPConnection("www.technologynet.co.ke")
-conn.request("GET", "/index.html")
-r1 = conn.getresponse()
-print r1.status, r1.reason
 
-data1 = r1.read()
-conn.request("GET", "/security.html")
-r2 = conn.getresponse()
-print r2.status, r2.reason
+def http_connects(site,page):
+ #checks if page is up print 200 ok response
+    connect = httplib.HTTPConnection(site)
+    connect.request("GET",page)
+    request1 = connect.getresponse()
+    print request1.status, request1.reason
 
-data2 = r2.read()
-#
-request = Request('http://technologynet.co.ke/index.html')
+    data1 = request1.read()
+    connect.request("GET", "/security.html")
+    request2 = connect.getresponse()
+    print request2.status, request2.reason 
 
-try:
-	response = urlopen(request)
-	rsp = response.read()
-	print rsp[559:1000]
-except URLError, e:
-    print 'an error occured:', e
-conn.close()
+    data2 = request2.read()
+    #Gets facebook startpage html source
+    request = Request('http://facebook.com/')
+
+    try:
+        response = urlopen(request)
+        rsp = response.read()
+        print rsp[559:1000]
+    except URLError, e:
+        print 'an error occured:', e
+    connect.close()
+
+http_connects("www.technologynet.co.ke", "/index.html")
